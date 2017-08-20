6c8e0118e1238b8d3c8e485be72ee747666a79d2
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2017-08-20 02:08:08 +0200

    Refactor tests to use Chai's expect instead of Chai's should

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,25 +1,22 @@
 import { suite, test } from 'mocha-typescript';
-import * as chai from 'chai';
+import { expect } from 'chai';
 
 import * as helpers from '../src/helpers';
 
-@suite()
+@suite
 class Helpers {
-    public static before() {
-        chai.should();
-    }
 
     @test('isSNSRecord should guard against non SNS messages')
     public testIsSnsRecord() {
-        helpers.isSnsRecord({EventSource: 'aws:sns'}).should.be.true;
-        helpers.isSnsRecord({EventSource: 'aws:ec2'}).should.be.false;
+        expect(helpers.isSnsRecord({ EventSource: 'aws:sns' })).to.be.true;
+        expect(helpers.isSnsRecord({ EventSource: 'aws:ec2' })).to.be.false;
     }
 
     @test('extractTopicInfo should be able to parse the SNS topic ARN')
     public testExtractTopicInfo() {
         const arn = 'arn:aws:sns:eu-west-1:00000000000:my_topic';
         const topicInfo = helpers.extractTopicInfo(arn);
-        topicInfo.region.should.equal('eu-west-1');
-        topicInfo.topic.should.equal('my_topic');
+        expect(topicInfo.region).to.equal('eu-west-1');
+        expect(topicInfo.topic).to.equal('my_topic');
     }
 }
